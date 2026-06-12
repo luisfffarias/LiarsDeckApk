c3d33b3c0148ab0515b9791054a26bb2115cca1d
Luis Fernando <luisfffarias27@gmail.com>
2026-06-12 16:48:06 -0300

    feat: build para web release

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/ubuntu/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ubuntu/projects/hello_world"
+export "FLUTTER_ROOT=C:\Users\luisf\dev\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\luisf\OneDrive\Área de Trabalho\Liars Deck\LiarsAPP\LiarsDeckApp"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\luisf\OneDrive\Área de Trabalho\Liars Deck\LiarsAPP\LiarsDeckApp\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,9 +6,23 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<audioplayers_darwin/AudioplayersDarwinPlugin.h>)
+#import <audioplayers_darwin/AudioplayersDarwinPlugin.h>
+#else
+@import audioplayers_darwin;
+#endif
+
+#if __has_include(<flutter_secure_storage/FlutterSecureStoragePlugin.h>)
+#import <flutter_secure_storage/FlutterSecureStoragePlugin.h>
+#else
+@import flutter_secure_storage;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AudioplayersDarwinPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioplayersDarwinPlugin"]];
+  [FlutterSecureStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSecureStoragePlugin"]];
 }
 
 @end
